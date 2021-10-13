125f8e51f28b1db5bdf33a702b84e949ae12deef
smallzhong <55563027+smallzhong@users.noreply.github.com>
2021-10-13 16:09:16 +0800

    配置完成环境

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,134 +1,191 @@
-﻿#include <iostream>
-#include <string>
-#include <cstring>
-#define PM 998244353
+﻿#include <stdio.h>
+#include <stdlib.h>
 
-using namespace std;
+#include "emu.h"
+#pragma comment(lib, "../unicorn.lib")
+#pragma comment(lib, "../capstone-4.0.2-win32/capstone_dll.lib")
+SEG_MAP segs[] = {
+	//base			size			file name
+	{0x00404000,	0x8c000,		"sample.vmp_00404000.bin",				NULL},
+	{0x0019B000,	0x5000,			"sample.vmp_0019B000.bin",				NULL},
+	{0x00403000,	0x1000,			"sample.vmp_00403000.bin",				NULL},
 
-int dp[100010][10];
-const string st = "nunhehheh";
+};
 
-int vis[100100];
-long long Anum[100010];
-long long nn[100010];
 
-long long pt(long long sum) {
-	if (sum == 1) {
-		return nn[sum];
+#define INIT_EAX			0x00000001
+#define INIT_EBX			0x00000000
+#define INIT_ECX			0x75105AA0
+#define INIT_EDX			0x751545D0
+#define INIT_EBP			0x0019FF70
+#define INIT_ESP			0x0019FF20
+#define INIT_ESI			0x75105584
+#define INIT_EDI			0x75105AD4
+#define INIT_EIP			0x00424596
+#define INIT_EFL			0x00000206
+
+uc_engine* uc;
+
+REGS regs;
+
+
+void print_stack(DWORD esp)
+{
+	DWORD val;
+	for (int i = 0; i < 10; i++)
+	{
+		uc_mem_read(uc, esp, &val, 4);
+		printf("|%p|\n", val);
+		esp += 4;
 	}
-	if (nn[sum]) return nn[sum];
-
-
-	return nn[sum] = pt(sum - 1) * sum;
 }
 
 
-int main()
+
+void read_regs()
 {
-	int T;
-	cin >> T;
-	nn[0] = 1;
-	for (int i = 1; i <= 1e5; i++) {
-		nn[i] = 2 * nn[i - 1];
-		nn[i] %= PM;
-	}
-	while (T--)
-	{
-		memset(vis, 0, sizeof(vis));
-		memset(Anum, 0, sizeof(Anum));
-		memset(dp, 0, sizeof(dp));
-		string s, ss;
-		cin >> s;
-
-		ss = s;
-		int pos = 0;
-		int num = 0;
-		dp[0][0] = 1;
-
-		//找出序列中有多少个存在子序列
-		for (int i = 0; i < ss.size() + 1; i++) {
-			dp[i][0] = 1;
-		}
-
-		for (int i = 1; i < ss.size() + 1; i++) {
-			for (int j = 1; j < st.size() + 1; j++) {
-				if (ss[i - 1] == st[j - 1])
-					dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-				else
-					dp[i][j] = dp[i - 1][j];
-			}
-		}
-
-
-		for (int i = 3; i < ss.size() + 1; i++) {
-			if (dp[i][st.size()] != dp[i - 1][st.size()]) {
-				vis[i] = dp[i][st.size()] - dp[i - 1][st.size()];
-			}
-		}
-		pos = 0;
-		num = 0;
+	uc_reg_read(uc, UC_X86_REG_EAX, &regs.regs.r_eax);
+	uc_reg_read(uc, UC_X86_REG_ECX, &regs.regs.r_ecx);
+	uc_reg_read(uc, UC_X86_REG_EDX, &regs.regs.r_edx);
+	uc_reg_read(uc, UC_X86_REG_EBX, &regs.regs.r_ebx);
+	uc_reg_read(uc, UC_X86_REG_ESP, &regs.regs.r_esp);
+	uc_reg_read(uc, UC_X86_REG_EBP, &regs.regs.r_ebp);
+	uc_reg_read(uc, UC_X86_REG_ESI, &regs.regs.r_esi);
+	uc_reg_read(uc, UC_X86_REG_EDI, &regs.regs.r_edi);
+	uc_reg_read(uc, UC_X86_REG_EIP, &regs.regs.r_eip);
+	uc_reg_read(uc, UC_X86_REG_EFLAGS, &regs.regs.r_efl);
+}
+void write_regs() {
+	uc_reg_write(uc, UC_X86_REG_EAX, &regs.regs.r_eax);
+	uc_reg_write(uc, UC_X86_REG_ECX, &regs.regs.r_ecx);
+	uc_reg_write(uc, UC_X86_REG_EDX, &regs.regs.r_edx);
+	uc_reg_write(uc, UC_X86_REG_EBX, &regs.regs.r_ebx);
+	uc_reg_write(uc, UC_X86_REG_ESP, &regs.regs.r_esp);
+	uc_reg_write(uc, UC_X86_REG_EBP, &regs.regs.r_ebp);
+	uc_reg_write(uc, UC_X86_REG_ESI, &regs.regs.r_esi);
+	uc_reg_write(uc, UC_X86_REG_EDI, &regs.regs.r_edi);
+	uc_reg_write(uc, UC_X86_REG_EIP, &regs.regs.r_eip);
+	uc_reg_write(uc, UC_X86_REG_EFLAGS, &regs.regs.r_efl);
+}
+void print_regs() {
+	printf("eax = %p\n", regs.regs.r_eax);
+	printf("ebx = %p\n", regs.regs.r_ebx);
+	printf("ecx = %p\n", regs.regs.r_ecx);
+	printf("edx = %p\n", regs.regs.r_edx);
+	printf("ebp = %p\n", regs.regs.r_ebp);
+	printf("esp = %p\n", regs.regs.r_esp);
+	printf("esi = %p\n", regs.regs.r_esi);
+	printf("edi = %p\n", regs.regs.r_edi);
+	printf("eip = %p\n", regs.regs.r_eip);
+	printf("efl = %p\n", regs.regs.r_efl);
+}
 
 
-		//确定每一个位置所含的a的数量
-		for (auto c : ss) {
-			if (num == 0) {
-				if (c == 'a') {
-					Anum[num] = 1;
-				}
-				else {
-					Anum[num] = 0;
-				}
-				num++;
-				continue;
-			}
-			if (c == 'a') {
-				Anum[num] = Anum[num - 1] + 1;
-			}
-			else Anum[num] = Anum[num - 1];
-			num++;
-		}
-		//       for(int i = 0; i < ss.size()+1; i++){
-		//            cout<<dp[i][st.size()]<<" ";
-		//       }
-		//        cout<<"\n";
-		//       for(int i = 0; i < ss.size()+1; i++){
-		//            cout<<Anum[i]<<" ";
-		//       }
-		//       cout<<"\n";
-		//
-		//       for(int i = 0; i < ss.size()+1; i++){
-		//            cout<<vis[i]<<" ";
-		//       }
-		//       cout<<"\n";
+int main(int argc, char** argv, char** envp)
+{
+	regs.regs.r_eax = INIT_EAX;
+	regs.regs.r_ecx = INIT_ECX;
+	regs.regs.r_edx = INIT_EDX;
+	regs.regs.r_ebx = INIT_EBX;
+	regs.regs.r_esp = INIT_ESP;
+	regs.regs.r_ebp = INIT_EBP;
+	regs.regs.r_esi = INIT_ESI;
+	regs.regs.r_edi = INIT_EDI;
+	regs.regs.r_eip = INIT_EIP;
+	regs.regs.r_efl = INIT_EFL;
 
-		num = 0;
-		long long ans = 0;
-		for (int i = 0; i < ss.size() + 1; i++) {
-			if (!vis[i]) continue;
-			//确定自某一为开始后a的数量
-			int sst = i;
-			int ssp = i - 1;
-			int skk = ssp + 1;
-			int sum = 0;//表示 a的数量
-			skk <= ss.size() - 1 ? sum = Anum[ss.size() - 1] - Anum[ssp] : sum = 0;
+	uc_err err;
+	csh handle;
+	cs_insn* insn;
 
+	printf("Emulate i386 code\n");
 
-
-			long long ansp = 1;
-			long long pp = 1;
-
-
-			//计算 方程
-			if (sum >= 1)
-				ansp = nn[sum] - 1;
-			else
-				continue;
-			ans += ((ansp * vis[i])) % PM;
-			ans %= PM;
-		}
-
-		cout << ans << "\n";
+	// Initialize emulator in X86-32bit mode
+	err = uc_open(UC_ARCH_X86, UC_MODE_32, &uc);
+	if (err != UC_ERR_OK) {
+		printf("Failed on uc_open() with error returned: %u\n", err);
+		return -1;
 	}
 
+	for (int i = 0; i < sizeof(segs) / sizeof(SEG_MAP); i++) {
+		segs[i].buf = (unsigned char*)malloc(segs[i].size);
+		FILE* fp = fopen(segs[i].file_name, "rb");
+		fread(segs[i].buf, segs[i].size, 1, fp);
+		fclose(fp);
+		// map memory for this emulation
+		err = uc_mem_map(uc, segs[i].base, segs[i].size, UC_PROT_ALL);
+		// write machine code to be emulated to memory
+		err = uc_mem_write(uc, segs[i].base, segs[i].buf, segs[i].size);
+		free(segs[i].buf);
+	}
+
+	if (cs_open(CS_ARCH_X86, CS_MODE_32, &handle)) {
+		printf("ERROR: Failed to initialize engine!\n");
+		return -1;
+	}
+	cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON);
+
+	write_regs();
+	init_gdt(uc);
+
+	BYTE code[32];
+	int count = 0;
+	// emulate code in infinite time & unlimited instructions
+	while (1) {
+		count++;
+		uc_mem_read(uc, regs.regs.r_eip, code, 32);
+		cs_disasm(handle, code, 32, regs.regs.r_eip, 1, &insn);
+
+		switch (regs.regs.r_eip)
+		{
+
+		case 0:
+			goto DEFAULT;
+		DEFAULT:
+		default:
+		{	/*DWORD val;
+			uc_mem_read(uc, 0x12f468, &val, 4);*/
+			if ( /*is_reg_tainted(X86_REG_EBP)*/
+				//regs.regs.r_ebp == INIT_EBP
+				//&& !strcmp(insn->mnemonic, "ret")
+				//&& is_reg_tainted(X86_REG_EDI)
+				//is_addr_tainted(0x662dc3)
+				//0
+				//count == 4118
+				0)
+			{
+				print_regs();
+				//print_taint_reg();
+				//print_taint_addr();
+				print_stack(regs.regs.r_esp);
+			}
+			//if (do_taint(insn))
+			//{
+			//	//printf("%p\t%s %s\n", regs.regs.r_eip, insn->mnemonic, insn->op_str);
+			//	//print_taint_reg();
+			//}
+		}
+		}
+
+		err = uc_emu_start(uc, regs.regs.r_eip, 0xffffffff, 0, 1);
+		if (err) {
+			printf("Exception with error returned %u: %s\n",
+				err, uc_strerror(err));
+			print_regs();
+			print_stack(regs.regs.r_esp);
+			__asm int 3
+			break;
+		}
+		read_regs();
+
+		cs_free(insn, 1);
+
+	}
+	printf("count = %d\n", count);
+
+	cs_close(&handle);
+	uc_close(uc);
+
+	system("pause");
 	return 0;
 }
